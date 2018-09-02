3a7e9819433e6896ec5e5983685f9dc0761811b7
woojiahao <woojiahao1234@gmail.com>
2018-09-02 14:35:40 +0800

    Initial commit

---
+++ app/src/main/java/chill/me/kotlintodo/Home.kt
@@ -0,0 +1,12 @@
+package chill.me.kotlintodo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
