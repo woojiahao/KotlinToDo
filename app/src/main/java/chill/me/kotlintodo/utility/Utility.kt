382b4071e16b3bc0fc7c1cb7a5c36067de90fa1a
woojiahao <woojiahao1234@gmail.com>
2018-09-30 13:29:30 +0800

    Added slides to the repo

--- app/src/main/java/chill/me/kotlintodo/utility/Utility.kt
+++ app/src/main/java/chill/me/kotlintodo/utility/Utility.kt
@@ -1,14 +1,6 @@
 package chill.me.kotlintodo.utility
 
-import android.content.Context
-import android.graphics.Color
-import android.widget.Toast
-import android.widget.Toast.LENGTH_SHORT
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 
-fun Context.toast(message: String) = Toast.makeText(this, message, LENGTH_SHORT).show()
-
 fun getCurrentDateTimeFormatted() = DateTimeFormat.forPattern("dd/MM/yyyy").print(DateTime.now())!!
-
-fun getDateTimeFormatted(dateTime: DateTime) = DateTimeFormat.forPattern("dd/MM/yyyy").print(dateTime)!!
